c4813267a99d56a7daa83b82cf475e3d439a88cb
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-07-12 11:23:04 +0500

    fetch data

---
+++ common.js
@@ -0,0 +1,12 @@
+const fetchApi = () => {
+    return fetch('https://api.example.com/data')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
+  };    
---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.write('hello sir');
+  res.end('Hello, World!\n');
+}
+
+);
+server.listen(8080)
