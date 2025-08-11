58d9094c3f20468f3d2c74f590fc29d5a4db5c4c
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-08-11 23:08:21 +0500

    small

--- common.js
+++ common.js
@@ -1,12 +1,15 @@
 const fetchApi = () => {
-    return fetch('https://api.example.com/data')
-    .then(response => {
+  return fetch("https://api.example.com/data")
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
     });
-  };    
+};
